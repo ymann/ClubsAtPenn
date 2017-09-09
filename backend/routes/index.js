f64c645a93e74235e89ad4acabca0fadaea158c9
ymann <ymann@wharton.upenn.edu>
2017-09-09 01:28:27 -0400

    Added MongoDB Library

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Yonah@Penn' });
+  res.render('index', { title: 'Yonah@Penn', condition: true, anyArray: [1,2,3] });
+});
+
+router.get('/:clubName', function(req, res, next) {
+    res.render('club', {title: req.params.clubName});
 });
 
 module.exports = router;
