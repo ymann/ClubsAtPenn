5cabc7342f2abb85d6d9cffa9af1e9c7886fa67a
bpiv400 <eplu@sas.upenn.edu>
2017-09-10 02:40:21 -0400

    validator

--- main/app.js
+++ main/app.js
@@ -4,33 +4,61 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var hbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var expressSession = require('express-session');
+var mongo = require('mongodb').MongoClient;
+var connectMongo = require('connect-mongo');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+var dbAccountInstance = mongo.conect("mongodb://localhost:27107/test", function(err, db) {
+    if(err) {
+        Console.log("Connection failed");
+    }
+    else {
+        Console.log("Account Connection Successful");
+    }
+});
+
 // view engine setup
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname +  '/views/'}));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator)());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressSession({secret: 'max', saveUninitialized: false, resave: false,
+    store: new MongoStore({db: dbAccountInstance})}));
 
 app.use('/', index);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
   next(err);
 });
 
